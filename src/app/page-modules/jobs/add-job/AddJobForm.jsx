b5721af3990656e94fcd311a6d74f38cd37029cf
tegarfaris <tegarfarisn@gmail.com>
2025-09-17 14:26:23 +0700

    feat: added breadcrumb on form add/edit page

--- src/app/page-modules/jobs/add-job/AddJobForm.jsx
+++ src/app/page-modules/jobs/add-job/AddJobForm.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import useJobs from "../../../hooks/useJobs";
 import { useParams } from "react-router-dom";
+import Breadcrumb from "../../../../components/breadcrumb/Breadcrumb";
 
 const JobForm = () => {
   const { pjobTypeId } = useParams();
@@ -67,71 +68,74 @@ const JobForm = () => {
   };
 
   return (
-    <div className="p-8 bg-white rounded-lg shadow border border-gray-200 w-full max-w-lg mx-auto">
-      <h2 className="text-2xl font-semibold text-gray-800 mb-6">
-        {pjobTypeId ? "Update Job" : "Add Job"}
-      </h2>
+    <div className="flex flex-col">
+      <Breadcrumb />
+      <div className="p-8 bg-white rounded-lg shadow border border-gray-200 w-full max-w-lg mx-auto">
+        <h2 className="text-2xl font-semibold text-gray-800 mb-6">
+          {pjobTypeId ? "Update Job" : "Add Job"}
+        </h2>
 
-      <div className="space-y-4">
-        {/* CODE */}
-        <div>
-          <label className="block text-sm font-medium text-gray-700 mb-1">
-            CODE
-          </label>
-          <input
-            type="text"
-            name="code"
-            placeholder="Enter CODE (max 128 chars)"
-            maxLength={128}
-            className="w-full p-2 border rounded focus:ring-2 focus:outline-none border-gray-300 focus:ring-blue-600"
-            value={form.code}
-            onChange={handleChange}
-          />
+        <div className="space-y-4">
+          {/* CODE */}
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              CODE
+            </label>
+            <input
+              type="text"
+              name="code"
+              placeholder="Enter CODE (max 128 chars)"
+              maxLength={128}
+              className="w-full p-2 border rounded focus:ring-2 focus:outline-none border-gray-300 focus:ring-blue-600"
+              value={form.code}
+              onChange={handleChange}
+            />
+          </div>
+
+          {/* Description */}
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              Description
+            </label>
+            <input
+              type="text"
+              name="description"
+              placeholder="Enter Description"
+              className="w-full p-2 border rounded focus:ring-2 focus:outline-none border-gray-300 focus:ring-blue-600"
+              value={form.description}
+              onChange={handleChange}
+            />
+          </div>
+
+          {/* Update By */}
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              Update By
+            </label>
+            <input
+              type="text"
+              name="updateBy"
+              placeholder="Enter Update By"
+              className="w-full p-2 border rounded focus:ring-2 focus:outline-none border-gray-300 focus:ring-blue-600"
+              value={form.updateBy}
+              onChange={handleChange}
+            />
+          </div>
         </div>
 
-        {/* Description */}
-        <div>
-          <label className="block text-sm font-medium text-gray-700 mb-1">
-            Description
-          </label>
-          <input
-            type="text"
-            name="description"
-            placeholder="Enter Description"
-            className="w-full p-2 border rounded focus:ring-2 focus:outline-none border-gray-300 focus:ring-blue-600"
-            value={form.description}
-            onChange={handleChange}
-          />
+        <div className="mt-6 flex justify-end">
+          <button
+            onClick={handleSubmit}
+            disabled={!isValid}
+            className={`px-5 py-2 rounded-md transition ${
+              isValid
+                ? "bg-gray-900 text-white hover:bg-blue-800"
+                : "bg-gray-400 text-gray-200 cursor-not-allowed"
+            }`}
+          >
+            {pjobTypeId ? "Update" : "Submit"}
+          </button>
         </div>
-
-        {/* Update By */}
-        <div>
-          <label className="block text-sm font-medium text-gray-700 mb-1">
-            Update By
-          </label>
-          <input
-            type="text"
-            name="updateBy"
-            placeholder="Enter Update By"
-            className="w-full p-2 border rounded focus:ring-2 focus:outline-none border-gray-300 focus:ring-blue-600"
-            value={form.updateBy}
-            onChange={handleChange}
-          />
-        </div>
-      </div>
-
-      <div className="mt-6 flex justify-end">
-        <button
-          onClick={handleSubmit}
-          disabled={!isValid}
-          className={`px-5 py-2 rounded-md transition ${
-            isValid
-              ? "bg-gray-900 text-white hover:bg-blue-800"
-              : "bg-gray-400 text-gray-200 cursor-not-allowed"
-          }`}
-        >
-          {pjobTypeId ? "Update" : "Submit"}
-        </button>
       </div>
     </div>
   );
