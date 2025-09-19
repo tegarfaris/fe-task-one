5a9522bf269010fc6dec5db47b95c57f29c3192e
tegarfaris <tegarfarisn@gmail.com>
2025-09-19 10:25:43 +0700

    feat: added for not found route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ProductsTable from "./app/page-modules/products/ProductsTable";
 import Wizard from "./app/page-modules/products/add-product/Wizard";
 import JobsTable from "./app/page-modules/jobs/JobsTable";
 import AddJobForm from "./app/page-modules/jobs/add-job/AddJobForm";
+import ComingSoon from "./components/coming-soon/ComingSoon";
 
 function App() {
   useEffect(() => {
@@ -23,6 +24,9 @@ function App() {
         <Route path="/job-list" element={<JobsTable />} />
         <Route path="/job-list/add-job" element={<AddJobForm />} />
         <Route path="/job-list/edit-job/:pjobTypeId" element={<AddJobForm />} />
+
+        {/* not found page */}
+        <Route path="*" element={<ComingSoon />} />
       </Routes>
     </Layout>
   );
